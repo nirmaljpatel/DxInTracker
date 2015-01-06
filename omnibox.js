0bb5a1fbd0133ae9b75f47b24397bb36667794ef
Nirmal <nirmaljpatel@gmail.com>
2015-01-07 02:12:25 +0530

    Omnibox integration

---
+++ omnibox.js
@@ -0,0 +1,54 @@
+/**************************************************************************
+ * Chrome Omnibox Integration
+ **************************************************************************/
+var suggestion = {
+	description: "Enter a ShipmentId to search on IPS site."
+ };
+chrome.omnibox.setDefaultSuggestion(suggestion);
+
+var chromeStorageKey = "DXTrkrIn_ShipmentIds";
+var searchOnIpsWeb = 'http://ipsweb.ptcmysore.gov.in/ipswebtracking/IPSWeb_item_events.asp?' +
+	  'Submit=Submit' + '&' +
+	  'itemid=';
+
+var findInArray = function(key, array) {
+	key = key.toUpperCase();
+	var results = [];
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].indexOf(key) == 0) {
+			results.push({content: array[i] + "", description: array[i] +" - Match from your saved ShipmentIds in DxInTracker."});
+		}
+	}
+	return results;
+}
+var StoredShipmentIds = [];
+// This event is fired only once for each input session.
+chrome.omnibox.onInputStarted.addListener(function(){
+	console.group("Omnibox.onInputStarted");
+	chrome.storage.sync.get([chromeStorageKey], function(result) {
+		console.log(result);
+		StoredShipmentIds = result.DXTrkrIn_ShipmentIds?result.DXTrkrIn_ShipmentIds:[];
+	});
+	console.log(StoredShipmentIds);
+	console.groupEnd();
+});
+// This event is fired each time the user updates the text in the omnibox,
+// as long as the extension's keyword mode is still active.
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    console.log('inputChanged: ' + text);
+	var savedMatches = findInArray(text, StoredShipmentIds);
+    suggest(savedMatches);
+  });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    console.log('inputEntered: ' + text);
+	//TODO - Offer to save this ShipmentId if not already in saved list
+	
+	chrome.tabs.create({
+		selected: true,
+		url: searchOnIpsWeb + text
+	});
+});
