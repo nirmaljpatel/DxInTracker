d3e8815256024373fe5cda4f9e8548499c7c58ff
Nirmal <nirmaljpatel@gmail.com>
2015-01-14 02:46:12 +0530

    use storageManager in omnibox.js and code improvements

--- omnibox.js
+++ omnibox.js
@@ -6,8 +6,6 @@ var suggestion = {
  };
 chrome.omnibox.setDefaultSuggestion(suggestion);
 
-//TODO:Code - Should not repeat these everywhere. Move them to a shared module.
-var chromeStorageKey = "DXTrkrIn_Shipments";
 var searchOnIpsWeb = 'http://ipsweb.ptcmysore.gov.in/ipswebtracking/IPSWeb_item_events.asp?' +
 	  'Submit=Submit' + '&' +
 	  'itemid=';
@@ -20,43 +18,62 @@ var findInArray = function(key, array, decorator) {
 	decorator = decorator || noDecorator;
 	key = key.toUpperCase();
 	var results = [];
-	for (var i = 0; i < array.length; i++) {
+	for (var i = 0; array && i < array.length; i++) {
 		if (array[i].shipmentId.indexOf(key) == 0) {
 			results.push(decorator(array[i]));
 		}
 	}
 	return results;
 }
+
+var buildOmniBoxSuggestResult = function(key, desc) {
+	var suggestResult = {
+		content: key,
+		description: desc
+	};
+	return suggestResult;
+};
 var StoredShipments = [];
+
 // This event is fired only once for each input session.
 chrome.omnibox.onInputStarted.addListener(function(){
 	console.group("Omnibox.onInputStarted");
-	chrome.storage.sync.get([chromeStorageKey], function(result) {
-		console.log(result);
-		StoredShipments = result.DXTrkrIn_Shipments?result.DXTrkrIn_Shipments:[];
-	});
+	storageManager.getAllShipments(function(shipments){
+			console.log("Got shipments:", shipments);
+			StoredShipments = shipments;
+		});
 	console.log(StoredShipments);
 	console.groupEnd();
 });
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
-chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    console.log('inputChanged: ' + text);
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+	console.group("omnibox.onInputChanged");
+	console.log('inputChanged: ' + text);
 	var savedMatches = findInArray(text, StoredShipments, function (shipment){
-		return {content: shipment.shipmentId + "", description: shipment.shipmentId +" ["+ shipment.label+"] from saved ShipmentIds in DxInTracker."};
+		return buildOmniBoxSuggestResult(shipment.shipmentId, shipment.shipmentId + " ["+(shipment.label?shipment.label:shipment.shippedOnDate)+"]");
 	});
-    suggest(savedMatches);
-  });
+	if(savedMatches.length == 0) {
+		savedMatches = [buildOmniBoxSuggestResult(" ", "No matches with saved shipments.")];
+	}
+	suggest(savedMatches);
+	console.groupEnd();
+});
 
 // This event is fired with the user accepts the input in the omnibox.
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    console.log('inputEntered: ' + text);
+chrome.omnibox.onInputEntered.addListener(function(text) {
+	console.group("omnibox.onInputEntered");
+	console.log('inputEntered: ' + text);
 	//TODO:Feature - Offer to save this ShipmentId if not already in saved list
 	//TODO:Feature - RegExp match to confirm its a ShipmentId
 	chrome.tabs.create({
 		selected: true,
 		url: searchOnIpsWeb + text
 	});
+	console.groupEnd();
+});
+
+chrome.omnibox.onInputCancelled.addListener(function(){
+	console.group("omnibox.onInputCancelled");
+	console.groupEnd();
 });
