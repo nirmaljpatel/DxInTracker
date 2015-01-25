014bc7bb22bb4b8fbf9a9d4e3d257b43e44ec921
Nirmal <nirmaljpatel@gmail.com>
2015-01-26 03:13:41 +0530

    npm run task to build chrome extension zip

---
+++ ZipBuilder.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+var archiver = require('archiver');
+
+var output = fs.createWriteStream('DxInTracker.zip');
+var archive = archiver('zip');
+
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+archive.bulk([
+  { expand: true, cwd:'src', src: ['**'], dest:'' }
+]);
+
+archive.finalize();
